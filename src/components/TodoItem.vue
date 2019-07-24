bcf04dd2e221a53dee5558564428f1a043f11417
Danny Zeng <DANNY.ZENG@oocl.com>
2019-07-24 17:55:18 +0800

    update some css style

--- src/components/TodoItem.vue
+++ src/components/TodoItem.vue
@@ -1,21 +1,19 @@
 <template>
   <div
     v-if="status==='All'||status==='Active'&&item.active===false||status==='Complete'&&item.active===true"
-    :class="[item.id%2===1?'odd-item':'even-item']"
+    :class="[contains(items, item)%2===1?'odd-item':'even-item']"
   >
-    <span>{{item.id}}.</span>
-    <a-checkbox v-model="item.active" />&nbsp;
-
+    <span>{{contains(items, item)+1}}.</span>
+    <a-checkbox v-model="item.active" />
+&nbsp;
     <label :class="{finishItem:item.active}" @dblclick="editItem(item.id)">
       <span v-if="!item.isEdit">{{item.name}}</span>
       <span v-else>
         <input v-model="item.name" type="text" @keyup.enter="finishEdit(item.id)" />
       </span>
     </label>
-    <a-icon type="close" style="float:right" @click="deleteItem(item.id-1)"/>
+    <a-icon type="close" style="float:right" @click="deleteItem(contains(items, item))" />
   </div>
-  
-  
 </template>
 
 <script>
@@ -25,6 +23,9 @@ export default {
   computed: {
     status() {
       return this.$store.getters.getStatus;
+    },
+    items() {
+      return this.$store.getters.getItems;
     }
   },
   methods: {
@@ -36,8 +37,17 @@ export default {
       this.$store.commit("finishEdit", id);
     },
 
-    deleteItem(id){
+    deleteItem(id) {
       this.$store.commit("remove", id);
+    },
+    contains(arrays, obj) {
+      var i = arrays.length;
+      while (i--) {
+        if (arrays[i] === obj) {
+          return i;
+        }
+      }
+      return false;
     }
   }
 };
